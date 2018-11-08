0070e7d476cfe513badfd266f2aecefd7bb688ab
Valentinas Kasteckis <41127259+Tortonas@users.noreply.github.com>
2018-11-08 20:54:47 +0200

    Add files via upload

--- INSERT THIS SQL TO MYSQL.sql
+++ INSERT THIS SQL TO MYSQL.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Oct 11, 2018 at 04:58 PM
+-- Generation Time: Nov 08, 2018 at 06:53 PM
 -- Server version: 5.5.27
 -- PHP Version: 5.6.38
 
@@ -51,7 +51,15 @@ INSERT INTO `LaikiuxAdmins` (`id`, `ip`, `nickname`, `level`, `steamid`) VALUES
 CREATE TABLE IF NOT EXISTS `LaikiuxHints` (
   `id` int(11) NOT NULL,
   `hinttext` varchar(1000) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `LaikiuxHints`
+--
+
+INSERT INTO `LaikiuxHints` (`id`, `hinttext`) VALUES
+(5, 'test hint'),
+(6, 'test hint');
 
 -- --------------------------------------------------------
 
@@ -74,7 +82,7 @@ CREATE TABLE IF NOT EXISTS `LaikiuxSubmission` (
 --
 
 INSERT INTO `LaikiuxSubmission` (`id`, `question`, `answer`, `prize`, `HasAnyoneWon`, `photolink`, `organizer`) VALUES
-(1, 'Kokioje Å¡alyje yra Å¡i pilis? Ats raÅ¡yti lietuviÅ¡kai', 'rumunija', '1000', '0', 'https://i.imgur.com/nfwP1Yg.jpg', 'Tortonas');
+(1, 'Klausimas nurodytas. Jeigu atsakymas per kablelÄ¯, bÅ«tina jÄ¯ atskirti taÅ¡ku vietoje kablelio. Pvz (0.68)', '0.829', 'VIP 1day', '1', 'https://i.imgur.com/NpWWGt2.png', 'Tortonas');
 
 -- --------------------------------------------------------
 
@@ -85,33 +93,19 @@ INSERT INTO `LaikiuxSubmission` (`id`, `question`, `answer`, `prize`, `HasAnyone
 CREATE TABLE IF NOT EXISTS `LaikiuxWinners` (
   `id` int(11) NOT NULL,
   `Nickname` varchar(1000) NOT NULL,
-  `organizer` varchar(1000) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=latin1;
+  `organizer` varchar(1000) NOT NULL,
+  `winnerIp` varchar(128) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=46 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `LaikiuxWinners`
 --
 
-INSERT INTO `LaikiuxWinners` (`id`, `Nickname`, `organizer`) VALUES
-(1, 'Re1den 1500 kreditai', ''),
-(11, 'DvitaÅ¡kis 1500 kreditai', ''),
-(12, 'DvitaÅ¡kis 1500 kreditai', ''),
-(14, 'Markas 1000 kreditai', ''),
-(16, 'zzz 1500 kreditai (sponsored)', ''),
-(17, 'XxX_DAnkmemer_xXx 2000 kreditai (sponsored)', ''),
-(18, 'Kappar 1500 kreditai', ''),
-(19, 'Kappar 1500 kreditai (sponsored)', ''),
-(20, 'DvitaÅ¡kis 2000 kreditai (sponsored)', ''),
-(21, 'DvitaÅ¡kis 1500 kreditai (sponsored)', ''),
-(22, 'Kappar 1500 kreditai (sponsored)', ''),
-(23, 'DvitaÅ¡kis 1500 kreditai', ''),
-(24, 'Re1Den 1000 kreditai', ''),
-(25, 'Markas 1500 kreditai', ''),
-(27, 'Slapyvardis 1500 kreditai', ''),
-(28, 'XxX_DAnkmemer_xXx 1000 kreditai', ''),
-(31, 'suva 1000 kreditai', 'Tortonas'),
-(32, 'XxX_DAnkmemer_xXx 1500 kreditai', 'In*Victus'),
-(33, 'suva 1500 kreditai', 'Tortonas');
+INSERT INTO `LaikiuxWinners` (`id`, `Nickname`, `organizer`, `winnerIp`) VALUES
+(34, 'Braskes Vaikas VIP 1day', 'Tortonas', ''),
+(35, 'In*Victus VIP 1day', 'Tortonas', ''),
+(36, 'Wez VIP 1day', 'Tortonas', ''),
+(37, 'Negras su raudonu bemwu VIP 1day', 'Tortonas', '');
 
 --
 -- Indexes for dumped tables
@@ -154,7 +148,7 @@ ALTER TABLE `LaikiuxAdmins`
 -- AUTO_INCREMENT for table `LaikiuxHints`
 --
 ALTER TABLE `LaikiuxHints`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT for table `LaikiuxSubmission`
 --
@@ -164,7 +158,7 @@ ALTER TABLE `LaikiuxSubmission`
 -- AUTO_INCREMENT for table `LaikiuxWinners`
 --
 ALTER TABLE `LaikiuxWinners`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=34;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=46;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
