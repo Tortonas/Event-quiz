123b52992740d445f3543457ba257f38b9deea6c
Valentinas Kasteckis <41127259+Tortonas@users.noreply.github.com>
2018-11-09 20:47:37 +0200

    Add files via upload

---
+++ includes/config.php
@@ -0,0 +1,4 @@
+<?php
+	date_default_timezone_set("Europe/Vilnius"); //default timezone http://php.net/manual/en/timezones.php
+	$WebDomain = "https://www.tortonas.tk"; // if you have SSL, use https, if you don't have it, use http.
+?>
