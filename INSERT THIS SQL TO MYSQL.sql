77641a2ca057182a4804a2c1d8f7d663f52f4397
Valentinas Kasteckis <41127259+Tortonas@users.noreply.github.com>
2019-01-20 20:45:41 +0200

    Add files via upload

--- INSERT THIS SQL TO MYSQL.sql
+++ INSERT THIS SQL TO MYSQL.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Dec 02, 2018 at 11:28 PM
+-- Generation Time: Jan 20, 2019 at 06:43 PM
 -- Server version: 5.5.27
 -- PHP Version: 5.6.38
 
@@ -56,23 +56,16 @@ CREATE TABLE IF NOT EXISTS `LaikiuxCredits` (
   `credits` int(11) NOT NULL,
   `nick` varchar(1000) NOT NULL,
   `bannedTill` datetime NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `LaikiuxCredits`
 --
 
 INSERT INTO `LaikiuxCredits` (`id`, `ip`, `credits`, `nick`, `bannedTill`) VALUES
-(3, 'playerIp', 0, '', '0000-00-00 00:00:00'),
-(4, 'playerIp', 0, '', '0000-00-00 00:00:00'),
-(5, 'playerIp', 0, '', '0000-00-00 00:00:00'),
-(6, 'playerIp', 2, 'Sparta', '2420-01-01 00:00:00'),
-(7, 'playerIp', 0, 'BoyNewToy', '0000-00-00 00:00:00'),
-(8, 'playerIp', 0, 'tRust', '0000-00-00 00:00:00'),
-(9, 'playerIp', 2, 'K3peja$', '2018-12-03 19:00:00'),
-(12, 'playerIp', 500, 'Tortonas', '2018-12-02 02:18:48'),
-(13, 'playerIp', 0, 'In*Victus', '2420-01-01 00:00:00'),
-(14, 'playerIp', 0, 'Haxters', '2420-01-01 00:00:00');
+(3, 'player ip', 0, '', '0000-00-00 00:00:00'),
+(37, 'player ip', 0, 'per lengva ', '2019-01-19 19:55:28'),
+(38, 'player ip', 1, 'jokiro', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -83,7 +76,7 @@ INSERT INTO `LaikiuxCredits` (`id`, `ip`, `credits`, `nick`, `bannedTill`) VALUE
 CREATE TABLE IF NOT EXISTS `LaikiuxHints` (
   `id` int(11) NOT NULL,
   `hinttext` varchar(1000) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -98,15 +91,17 @@ CREATE TABLE IF NOT EXISTS `LaikiuxQuestions` (
   `prize` varchar(1000) NOT NULL,
   `photolink` varchar(1000) NOT NULL,
   `organizer` varchar(1000) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=51 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `LaikiuxQuestions`
 --
 
 INSERT INTO `LaikiuxQuestions` (`id`, `question`, `answer`, `prize`, `photolink`, `organizer`) VALUES
-(6, 'Is kurio filmo yra sis pagrindinis veikejas?(Angliskai)', 'answer', 'VIP 2days', 'https://i.imgur.com/****2.png', 'Haxters'),
-(14, 'question', 'ans', 'VIP 2days', 'https://i.imgur.com/*****.png', 'Haxters');
+(47, 'ApskaiÄiuokite X dispersijÄ….  Ats pvz 1.23', '0.84', 'VIP 2days', 'https://i.imgur.com/UNkiK1r.png', 'Tortonas'),
+(48, 'ApskaiÄiuokite Y vidurkÄ¯ ats pvz.: 1.1', '0.8', 'VIP 2days', 'https://i.imgur.com/MwbtIS3.png', 'Tortonas'),
+(49, 'ApskaiÄiuokite Y dispersijÄ….  Ats pvz 1.23', '0.76', 'VIP 2days', 'https://i.imgur.com/mUbGXuN.png', 'Tortonas'),
+(50, 'ApskaiÄiuokite kovariacijÄ… pagal x,y. Ats pvz.: 0.45', '0.08', 'VIP 2days', 'https://i.imgur.com/pVai9RR.png', 'Tortonas');
 
 -- --------------------------------------------------------
 
@@ -130,7 +125,7 @@ CREATE TABLE IF NOT EXISTS `LaikiuxSubmission` (
 --
 
 INSERT INTO `LaikiuxSubmission` (`id`, `question`, `answer`, `prize`, `HasAnyoneWon`, `photolink`, `organizer`, `dateTime`) VALUES
-(1, 'Kokio seno s. admino profilis pavaizduotas? Atsakymas vardininkas.', 'Vyskupas', 'VIP 2days', '1', 'https://i.imgur.com/aBVz7gN.png', 'Tortonas', '2018-12-03 18:00:00');
+(1, 'ApskaiÄiuokite vidurkÄ¯ ats pvz.: 1.1', '0.4', 'VIP 2days', '0', 'https://i.imgur.com/eUsXpjS.png', 'Tortonas', '2019-01-21 18:00:00');
 
 -- --------------------------------------------------------
 
@@ -143,16 +138,17 @@ CREATE TABLE IF NOT EXISTS `LaikiuxWinners` (
   `Nickname` varchar(1000) NOT NULL,
   `organizer` varchar(1000) NOT NULL,
   `winnerIp` varchar(128) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=88 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=131 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `LaikiuxWinners`
 --
 
 INSERT INTO `LaikiuxWinners` (`id`, `Nickname`, `organizer`, `winnerIp`) VALUES
-(34, 'Braskes Vaikas VIP 1day', 'Tortonas', 'userIp'),
-(35, 'In*Victus VIP 1day', 'Tortonas', 'userIp'),
-(81, 'K3peja$ VIP 2days', 'Tortonas', 'userIp');
+(34, 'Braskes Vaikas VIP 1day', 'Tortonas', 'player ip'),
+(128, 'tabak VIP 2days', 'Tortonas', 'player ip'),
+(129, 'tabak VIP 2days', 'Tortonas', 'player ip'),
+(130, 'per lengva  VIP 2days', 'Tortonas', 'player ip');
 
 --
 -- Indexes for dumped tables
@@ -207,17 +203,17 @@ ALTER TABLE `LaikiuxAdmins`
 -- AUTO_INCREMENT for table `LaikiuxCredits`
 --
 ALTER TABLE `LaikiuxCredits`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=39;
 --
 -- AUTO_INCREMENT for table `LaikiuxHints`
 --
 ALTER TABLE `LaikiuxHints`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT for table `LaikiuxQuestions`
 --
 ALTER TABLE `LaikiuxQuestions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=51;
 --
 -- AUTO_INCREMENT for table `LaikiuxSubmission`
 --
@@ -227,7 +223,7 @@ ALTER TABLE `LaikiuxSubmission`
 -- AUTO_INCREMENT for table `LaikiuxWinners`
 --
 ALTER TABLE `LaikiuxWinners`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=88;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=131;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
